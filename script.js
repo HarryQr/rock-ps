f78585b79be1a01a6d58ee016227a93279f67494
HarryQr <96742459+HarryQr@users.noreply.github.com>
2023-08-27 04:31:01 +0300

    Add rock-ps

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-//Project: Rock Paper Scissors
-
-//rock button
-const rockBtn = document.querySelector(#rock);
-rockBtn.classList.add("playButtons");
-
-//Paper Button
-const paperBtn = document.querySelector(#paper);
-paperBtn.classList.add("playButton");
-
-//Scissor Button
-const ScissorsBtn = document.querySelector(#scissors);
-ScissorsBtn.classList.add("palayButton");
-
-//New Game Button
-const newGameBtn = document.querySelector(#newGame);
-newGameBtn.classList.add("newGameButton");
-
-const roundScore = document.querySelector("#roundScore");
-const finalScore = document.querySelector("#finalScore");
-const player = document.querySelector("#player");
-const computer = document.querySelector("#computer");
-
-//function randomly returns "Rock", "Paper", or "Scissors"
-
+function computerPlay() {
+    let rps = ["rock", "paper", "scissors"];
+    let random = rps[Math.floor(Math.random() * rps.length)];
+    return random;
+  }
+  
+  console.log(computerPlay());
+  
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return ("It's a draw!");
+    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+      return ("You win! Rock beats scissors");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+      return ("You lose! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      return ("You win! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      return ("You lose! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      return ("You win! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      return ("You lose!Rock beats scissors");
+    }
+  
+  }
+  
+  let computerSelection = computerPlay();
+  let playerSelection = prompt("Choose your weapon");
+  console.log(playRound(playerSelection, computerSelection));
