8ff579436bdd8efdf7de990f0757ed3af97579ed
HarryQr <96742459+HarryQr@users.noreply.github.com>
2023-08-27 04:07:34 +0300

    Add rock-ps

---
+++ script.js
@@ -0,0 +1,25 @@
+//Project: Rock Paper Scissors
+
+//rock button
+const rockBtn = document.querySelector(#rock);
+rockBtn.classList.add("playButtons");
+
+//Paper Button
+const paperBtn = document.querySelector(#paper);
+paperBtn.classList.add("playButton");
+
+//Scissor Button
+const ScissorsBtn = document.querySelector(#scissors);
+ScissorsBtn.classList.add("palayButton");
+
+//New Game Button
+const newGameBtn = document.querySelector(#newGame);
+newGameBtn.classList.add("newGameButton");
+
+const roundScore = document.querySelector("#roundScore");
+const finalScore = document.querySelector("#finalScore");
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+
+//function randomly returns "Rock", "Paper", or "Scissors"
+
